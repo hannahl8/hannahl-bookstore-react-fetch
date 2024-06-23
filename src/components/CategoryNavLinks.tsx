550cf050189b482c687ee78736183ca5dd4514fe
hannahl8 <hannahl8@vt.edu>
2024-06-23 12:35:08 -0400

    project 5 complete

--- src/components/CategoryNavLinks.tsx
+++ src/components/CategoryNavLinks.tsx
@@ -1,12 +1,16 @@
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import {CategoryProps} from "../types.ts";
-export default function CategoryNavLinks(props: CategoryProps) {
-  // const categoryList = getCategories();
-  const categoryNavLinks = props.categories.map((category) => (
-    <li key={category.categoryId}>
-      <NavLink to={`/category/${category.name}`}>{category.name} <i className={category?.icon}></i></NavLink>
-    </li>
-  ));
+import {icons} from "../utils.ts";
 
-  return <ul>{categoryNavLinks}</ul>;
+export default function CategoryNavLinks(props: CategoryProps) {
+    const categoryNavLinks = props.categories.map((category) => {
+        const icon = category.name in icons ? icons[category.name] : '';
+        return (
+            <li key={category.categoryId}>
+                <NavLink to={`/category/${category.name}`}>{category.name} <i className={icon}></i></NavLink>
+            </li>
+        );
+    });
+
+    return <ul>{categoryNavLinks}</ul>;
 }
